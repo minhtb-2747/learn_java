606a3ac398e7a6b262389a1b1b7160456a3a19aa
minhtb-2747 <tran.binh.minh@sun-asterisk.com>
2026-09-22 10:48:32 +0700

    feat(lab1): add hello api

---
+++ java_spring_framework/employee_management/src/test/java/com/employee_management/employee_management/controller/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.employee_management.employee_management.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello world !"));
+    }
+}
