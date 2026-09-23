c1468d31ae75eec1de180d0eb49361116754d6f8
minhtb-2747 <tran.binh.minh@sun-asterisk.com>
2026-09-23 14:47:32 +0700

    feat: Scheduled task log ra console mỗi 30s với nội dung “System running”

---
+++ java_spring_framework/employee_management/src/main/java/com/employee_management/employee_management/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.employee_management.employee_management.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
